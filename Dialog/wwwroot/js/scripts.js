ee7b9639280b172477a47432a2fc76efa5820055
Joseph Tomlinson and Rakhee Gandhi <joseph+rakhee@>
2017-11-09 13:03:26 -0800

    fix minor routing issue

--- Dialog/wwwroot/js/scripts.js
+++ Dialog/wwwroot/js/scripts.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     var stringDate = $(datediv).attr("value")
 
     var date = new Date(stringDate + " UTC")
-    console.log(date);
     var localeString = date.toLocaleString()
     localeString = localeString.replace(", ", " </br> ")
     $(datediv).append("Posted: " + localeString);
